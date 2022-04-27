8a20664c884cf85093ea87a9b68d1516933b126d
LukeSnywalker <lukesnyder98@gmail.com>
2022-04-27 10:29:26 -0400

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -15,8 +15,12 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis.
-    print(election_data)
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
 
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
